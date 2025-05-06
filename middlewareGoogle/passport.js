04552e268c11c3545f44c80ac14c0fde2f9066ed
Allison12B <allisonblancorodriguez@gmail.com>
2025-05-06 17:45:14 -0600

    Create passport

---
+++ middlewareGoogle/passport.js
@@ -0,0 +1,28 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+import User from '../models/userModel.js';
+
+dotenv.config();
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/api/auth/google/callback"
+}, async (accessToken, refreshToken, profile, done) => {
+  let user = await User.findOne({ googleId: profile.id });
+  if (!user) {
+    user = await User.create({
+      googleId: profile.id,
+      email: profile.emails[0].value,
+      name: profile.displayName
+    }); 
+  }
+  done(null, user);
+}));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
